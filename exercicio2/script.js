9093aae37abb502493a3dc9c38485c9fa6aef85e
myllenavieira <vieirasilvamyllena@gmail.com>
2022-07-25 21:47:27 -0300

    Exercícios para praticar A009

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,9 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+let minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+console.log("Caracteres antes:", minhaString.length);
+minhaString = minhaString.trim()
+console.log("Caracteres depois:",minhaString.length);
+minhaString = minhaString.replace("________", "sticioso");
+console.log(minhaString);
+
+// Mudei a string do tipo const para let para simplificar o código e facilitar as operações
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,12 @@
-//Crie a const para a frase aqui
+const frase = `Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"`
+let fraseModificada = frase.replaceAll("verde", "rosa");
+fraseModificada = fraseModificada.replaceAll("azul", "laranja");
+
+console.log(fraseModificada);
+console.log(`A frase incluí verde? ${fraseModificada.includes("verde")} 
+A frase incluí laranja? ${fraseModificada.includes("laranja")}`);
+
+// fraseModificada = fraseModificada.replaceAll("mas não deixe o gato sair", "MAS NÃO DEIXE O GATO SAIR");
+let paraMaiusculo = "mas não deixe o gato sair";
+fraseModificada = fraseModificada.replaceAll(paraMaiusculo, paraMaiusculo.toUpperCase());
+console.log(fraseModificada);
